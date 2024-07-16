46e067750d8916a0f859d84602e387dcd249e2a2
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-07-17 01:30:34 +0400

    feat: add title to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import mainImage from '@/app/ui/images/main.png';
+import { ceraPro } from './ui/fonts';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.image}></div>
+      <div className={styles.image}>
+        <div className={styles.text}>
+          <p className={ceraPro.className}>Ut enim ad minim veniam</p>
+          <h1 className={ceraPro.className}>Duis aute irure dolor in reprehenderit</h1>
+          <h2 className={ceraPro.className}>Duis aute irure dolor in reprehenderit</h2>
+        </div>
+      </div>
     </main>
   );
 }
