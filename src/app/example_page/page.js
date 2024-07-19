14947b98a55667f7bd0658dce9729986cb3725c5
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-07-19 15:51:48 +0400

    style: add title animation

--- src/app/example_page/page.js
+++ src/app/example_page/page.js
@@ -6,9 +6,11 @@ export default function Page() {
     <main className={styles.main}>
       <div className={styles.image}>
         <div className={styles.text}>
-          <p className={ceraPro.className}>Ut enim ad minim veniam</p>
+          <p className={ceraPro.className}>It's neccessary for testing animation</p>
           <h1 className={ceraPro.className}>This is an example page</h1>
-          <h2 className={ceraPro.className}>It's neccessary for testing animation</h2>
+          <h2 className={ceraPro.className}>
+            Сlick on the title or logo to return to the home page
+          </h2>
         </div>
       </div>
     </main>
